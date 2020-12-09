71077a6738edd95f9483f38e265b63f4fe3d86dc
Prestopine_Connor <Prestopine_Connor@student.mahoningctc.com>
2020-12-09 13:11:21 -0500

    JavaScript Output

---
+++ JS Output Challenge/script.js
@@ -0,0 +1,36 @@
+// Enter your code below to edit index.html
+// Name
+yourName = "Connor Prestopine"
+document.getElementById("yourName").innerHTML = yourName;
+
+// About me 
+myBio = "Hi my name is Connor, and I like Cars and Business, and I'm learning Softwear Engineering at MCCTC."
+document.getElementById("myBio").innerHTML = myBio;
+// college choice
+collegeChoice = " I want to go to Seoul National University."
+document.getElementById("collegeChoice").innerHTML = collegeChoice;
+
+programChoice = " I want to major in business ."
+document.getElementById("programChoice").innerHTML = programChoice;
+
+collegeReason = " I want to major in business because I want to own my own tech business one day so to learn how to manage it."
+document.getElementById("collegeReason").innerHTML =collegeReason;
+
+careerChoice = "CEO"
+document.getElementById("careerChoice").innerHTML =careerChoice;
+
+companyChoice = "I want to be the CEO of my own company"
+document.getElementById("companyChoice").innerHTML =companyChoice;
+
+careerReason = "I want to be the CEO of my own company beacuse I have always hated the idea of working for someone else, but it isn't just that I want to start a tech company that can inavate the work as well as helping people that are handicaped such as not able to see talk and or drive. I want to make the world easier for people that don't have the odds against them even if I can just help alittle bit It will mean that I have compleated my mission."
+document.getElementById("careerReason").innerHTML =careerReason;
+
+answerOne = "The Document Object Model is a cross-platform and language-independent interface that treats an XML or HTML document as a tree structure wherein each node is an object representing a part of the document. The DOM represents a document with a logical tree."
+document.getElementById("answerOne").innerHTML = answerOne;
+
+answerTwo = "You can use innerHTML, document.write(), window.write(), or console.log()"
+document.getElementById("answerTwo").innerHTML = answerTwo;
+
+answerthree = "JavaScript can change all HTML elements in the page"
+document.getElementById("answerThree").innerHTML = answerThree;
+
